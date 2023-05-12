6f2884d7d5a15ff1e1fba09a4c29aa2cd3e4c318
mathewgurney <mathew@icons.studio>
2023-05-12 17:54:04 +0100

    Added heading to export file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { Flex } from "./components/Flex";
+export { Heading } from './components/Heading';
 export { Paragraph } from "./components/Paragraph";
 export { Container } from "./components/Container";
 export { Section } from "./components/Section";
